fbc14dc0b159653bbc49e09c1c0daa67c381111d
lilia <lilianom59@outlook.fr>
2022-11-30 15:11:34 +0100

    string2

--- exo.js
+++ exo.js
@@ -1,3 +1,24 @@
 let pomme = "Elle est belle ma pomme !";
 
-console.log(pomme.lastIndexOf('m'));
+console.log(pomme.indexOf('pomme'));
+
+document.getElementById('last-index-m').innerHTML = pomme.lastIndexOf('m');
+
+
+if (pomme.startsWith('Elle')) {
+    console.log('Commence bien par : Elle')
+}
+
+
+if (pomme.endsWith('!')) {
+    console.log("Termine bien par un point d'exclamation")
+}
+
+
+let pomme2 = pomme.substring(17, 23);
+document.getElementById('pomme').innerHTML = pomme2;
+
+
+for (let i = 0; i < pomme.length; i++){
+    document.getElementById('one-line').innerHTML += pomme[i] + '<br>';
+}
